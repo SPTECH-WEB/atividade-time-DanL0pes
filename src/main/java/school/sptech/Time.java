87028cf6a8ec8b60ee8ab958c427477f0adaa640
DanL0pes <daniel.sousa@sptech.school>
2025-08-29 12:53:05 -0300

    Criando Construtor da classe Time

--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,13 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    int vitorias, empates, derrotas;
 
+    public void Time(String nome){
+        this.nome = nome;
+        vitorias = 0;
+        derrotas = 0;
+        empates = 0;
+    }
 }
